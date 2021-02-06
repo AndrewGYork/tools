d3a2a391543497a828f540baf849006971da0d5d
Alfred Millett-Sikking <amsikking@gmail.com>
2021-02-05 17:04:00 -0800

    Update thorlabs.py
    General tidy up + additional features:
    - 'settle_time' argument added to the 'set_voltage' method for piezo/mechanics to settle after motion (should be tuned for each setup)
    - non-blocking 'set_voltage' + '_finish_set_voltage' method for reduced latency when making a series of calls to difference devices

--- thorlabs.py
+++ thorlabs.py
@@ -2,23 +2,17 @@ import time
 import serial
 
 class MDT694B_piezo_controller:
-    def __init__(
-        self,
-        port_number=2, # COM3 is port 2. Windows/Python mismatch?
-        verbose=True
-        ):
+    def __init__(self, which_port, verbose=True):
         self.verbose = verbose
         if self.verbose: print("Opening piezo...", end='')
-        self.port = serial.Serial(port=port_number, baudrate=115200)
-        # Restore the piezo controller to default settings
-        self.port.write(b'restore\n')
+        self.port = serial.Serial(port=which_port, baudrate=115200)
+        self.port.write(b'restore\n') # reset controller to default settings
         response = self.port.read(51)
         assert response == (
             b'restore\n*All settings restored to default values.\r*')
         assert self.port.inWaiting() == 0
         if self.verbose: print(" done.")
-        # Check what the voltage limit of the piezo controller is set to
-        self.port.write(b'vlimit?\n')
+        self.port.write(b'vlimit?\n') # check voltage limit set point
         response = self.port.read(16)
         assert self.port.inWaiting() == 0
         limit_settings = {b'vlimit?\n*[  75]\r': 75,
@@ -27,21 +21,37 @@ class MDT694B_piezo_controller:
         assert response in limit_settings
         self.voltage_limit_setting = limit_settings[response]
         if self.verbose:
-            print(" Piezo voltage limit setting:",
+            print("Piezo voltage limit setting:",
                   self.voltage_limit_setting)
+        self._pending_cmd = None
+        self._settle_time = None
         return None
 
-    def set_voltage(self, v):
-        v = float(v)
-        assert 0 <= v <= self.voltage_limit_setting
+    def set_voltage(self, voltage, settle_time=None, block=True):
+        if self._pending_cmd is not None:
+            self._finish_set_voltage()
+        voltage = float(voltage)
+        assert 0 <= voltage <= self.voltage_limit_setting
         if self.verbose:
-            print("Setting piezo voltage to %0.2f volts..."%v, end='')
-        command = ('xvoltage=%0.2f\n'%v).encode('ascii')
-        self.port.write(command)
-        response = self.port.read(len(command) + 1)
-        assert response == command + b'*'
+            print("Setting piezo voltage to %0.2f volts..."%voltage)
+        cmd = ('xvoltage=%0.2f\n'%voltage).encode('ascii')
+        self.port.write(cmd)
+        self._pending_cmd = cmd
+        if settle_time is not None: self._settle_time = float(settle_time)
+        if block:
+            self._finish_set_voltage()
+        return None
+
+    def _finish_set_voltage(self):
+        if self._pending_cmd is None:
+            return
+        response = self.port.read(len(self._pending_cmd) + 1)
+        assert response == self._pending_cmd + b'*'
         assert self.port.inWaiting() == 0
-        if self.verbose: print(" done.")
+        if self._settle_time is not None:
+            time.sleep(self._settle_time) # for piezo and mechanics to settle
+        self._pending_cmd = None
+        if self.verbose: print("Done setting piezo voltage.")
         return None
 
     def close(self):
@@ -51,11 +61,33 @@ class MDT694B_piezo_controller:
         return None
 
 if __name__ == '__main__':
-    # Simple test code to show that the piezo controller is working
-    piezo = MDT694B_piezo_controller()
-    for i in range(10):
-        piezo.set_voltage(1)
-        time.sleep(0.2)
-        piezo.set_voltage(2)
-        time.sleep(0.2)
+    # init:
+    start = time.perf_counter()
+    piezo = MDT694B_piezo_controller('COM7', verbose=True)
+    print('Initialze time (s):', time.perf_counter() - start)
+
+    piezo.verbose = False
+    
+    # regular call:
+    start = time.perf_counter()
+    piezo.set_voltage(0)
+    print('Set voltage time (s):', time.perf_counter() - start)
+
+    # non blocking call:
+    start = time.perf_counter()
+    piezo.set_voltage(0, block=False)
+    print('Set voltage non-blocking (s):', time.perf_counter() - start)
+
+    # non blocking call + finish:
+    start = time.perf_counter()
+    piezo.set_voltage(0, block=False)
+    piezo._finish_set_voltage()
+    print('Set voltage non-blocking + finish (s):', time.perf_counter() - start)
+
+    # settle time:
+    piezo.verbose = True
+    for i in range(5):
+        piezo.set_voltage(0, settle_time=0.1)
+        piezo.set_voltage(5, settle_time=0.5)
+
     piezo.close()
